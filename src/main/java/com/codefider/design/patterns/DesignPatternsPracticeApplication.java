3544599d572f3fd57ae1ca7f325368f18f30d0a5
hasnainmd <hasnain2win@gmail.com>
2024-11-07 19:05:31 +0530

    pom pushed

---
+++ src/main/java/com/codefider/design/patterns/DesignPatternsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codefider.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefider/design/patterns/DesignPatternsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefider.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
